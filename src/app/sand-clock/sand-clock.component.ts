e0dca42dbb193f9c02471679379439fd9c2fdb6e
davidpaz06 <davidalbertopaz@gmail.com>
2024-11-19 22:40:22 -0400

    avance

--- src/app/sand-clock/sand-clock.component.ts
+++ src/app/sand-clock/sand-clock.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimeService } from '../../services/time.service';
 
 @Component({
   selector: 'app-sand-clock',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      sand-clock works!
-    </p>
+  template: ` <p>sand-clock works!</p> `,
+  styles: `
   `,
-  styles: ``
 })
-export class SandClockComponent {
+export class SandClockComponent implements OnInit {
+  systemTime: string = '';
+  constructor(private timeService: TimeService) {}
 
+  ngOnInit() {
+    this.timeService.systemTime$.subscribe((time) => {
+      this.systemTime = time;
+    });
+  }
 }
